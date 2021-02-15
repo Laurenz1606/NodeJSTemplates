6e099eabd7401fd3016c756a3b42b250b1273bd3
Laurenz1606 <laurenz.rausche@gmail.com>
2021-02-15 20:22:32 +0100

    paginated-api

--- paginated-api/app.js
+++ paginated-api/app.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 
 //dependencies
 const express = require('express')
+const cors = require('cors')
 
 //express config
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 app.listen(process.env.PORT, () => console.log(`Part running on Port ${process.env.PORT}`))
