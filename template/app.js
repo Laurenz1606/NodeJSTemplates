24dfb88a09a3b3c5a097b91ae1aa95247b7ad366
Laurenz1606 <laurenz.rausche@gmail.com>
2021-02-15 20:25:38 +0100

    template

--- template/app.js
+++ template/app.js
@@ -7,4 +7,5 @@ const express = require('express')
 //express config
 const app = express()
 
+//listen to the port
 app.listen(process.env.PORT, () => console.log(`Part running on Port ${process.env.PORT}`))
