75af4b222e5463d203a198cd9591677c69fdc813
Laurenz1606 <laurenz.rausche@gmail.com>
2021-02-17 17:46:49 +0100

    session-login-temp

--- session-login-temp/passport-config.js
+++ session-login-temp/passport-config.js
@@ -1,24 +1,38 @@
+//dependencies
 const LocalStrategy = require("passport-local").Strategy
 const bcrypt = require("bcrypt")
 
+//initialize a passport session
 function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = (email, password, done) => {
+  //authenticate a user
+  const authenticateUser = async (email, password, done) => {
+    //get a user by his email(function in app.js)
     const user = getUserByEmail(email)
-    if (user == null) {
-      return done(null, false, { message: "No user with that email" })
-    }
-    if (bcrypt.compareSync(password, user.password)) {
-      return done(null, user)
-    } else {
-      return done(null, false, { message: "Password incorrect" })
+
+    //check if user exists
+    if (user == null) return done(null, false, { message: "No user with that email" })
+
+    try {
+      //check if password matches else return an error
+      if (await bcrypt.compare(password, user.password)) return done(null, user)
+
+      //returning the error
+      else return done(null, false, { message: "Password incorrect" })
+    } 
+    
+    //catch errors that occur
+    catch (e) {
+      return done(e)
     }
   }
 
-  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser))
+  //passport logic
+  passport.use(new LocalStrategy({ usernameField: "email", passwordField: "password" }, authenticateUser));
   passport.serializeUser((user, done) => done(null, user.id))
   passport.deserializeUser((id, done) => {
     return done(null, getUserById(id))
   })
 }
 
+//export the code1
 module.exports = initialize
